1e5dad61723b247657c7a4f33621545cbe556f46
vlvkdn4107 <vlvkdn4107@gamil.com>
2022-05-10 17:46:27 +0900

    mysql기능만들기

--- db_connect/src/ch04/MainTest.java
+++ db_connect/src/ch04/MainTest.java
@@ -8,16 +8,16 @@ public class MainTest {
 
 
 		EmployeesDao employeesDao = new EmployeesDao();
-//		ArrayList<EmployeesDto> data = employeesDao.select();
-//		System.out.println(data);
+		ArrayList<EmployeesDto> data = employeesDao.select();
+		System.out.println(data);
 //		ArrayList<EmployeesSelectDto> data = employeesDao.employeesSelect("Marketing", "9999-01-01");
 //		System.out.println(data);
 //		ArrayList<EmployeesSelectDto> data = employeesDao.managerSelect("Finance");
 //		System.out.println(data);
 //		ArrayList<TitleEmployeesDto> data = employeesDao.titleEmployees("Senior Engineer");
 //		System.out.println(data);
-		ArrayList<GenderSelectDto> data = employeesDao.genderSelecet("M");
-		System.out.println(data);
+//		ArrayList<GenderSelectDto> data = employeesDao.genderSelecet("M");
+//		System.out.println(data);
 		
 	}
 
